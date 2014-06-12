406bb45e89c2e224499006c4dfd6e48e9515f68c
Jean-Louis Fuchs <ganwell@fangorn.ch>
2014-06-13 00:25:28 +0200

    My big test

--- benchmark.py
+++ benchmark.py
@@ -28,34 +28,39 @@ import asyncio
 import importlib.machinery  # noqa
 import os
 import random
+import dbm
 
 loader = importlib.machinery.SourceFileLoader(
     "kyototycoon", "files/kyototycoon_orig.py"
 )
 kyototycoon = loader.load_module()
 
-NUM_REQUESTS = 1000
 NUM_BULK = 50
+NUM_REQUESTS = int(100000000 / NUM_BULK)
 NUM_BATCH = 20
 
 
 loop = asyncio.get_event_loop()
 
-#client = ktasync.KyotoTycoon.embedded(["test.kch"])
-client = ktasync.KyotoTycoon(host="harry")
-orig   = kyototycoon.KyotoTycoon(host=client.host, port=client.port)
+client   = ktasync.KyotoTycoon.embedded(["test.kch"])
+#client   = ktasync.KyotoTycoon(host="harry")
+orig     = kyototycoon.KyotoTycoon(host=client.host, port=client.port)
+dbm_file = dbm.open("test.dbm", "c")
 
 
 def _create_request():
     """Get requests"""
     a = os.urandom(64)
-    c = random.randint(0, 1024)
-    b = os.urandom(1)
-    return [
-        {
-            a : b * 20 for n in range(NUM_BULK)
-        } for n in range(NUM_REQUESTS)  # noqa
-    ]
+    c = random.randint(1024, 1024*20)
+    b = os.urandom(c)
+    arr = []
+    for _ in range(NUM_REQUESTS):
+        dict_ = {}
+        for _ in range(NUM_BULK):
+            dict_[a] = b
+        arr.append(dict_)
+    return arr
+
 
 requests = _create_request()
 
@@ -148,6 +153,35 @@ def benchmark_orig_get_bulk():
     )
 
 
+def benchmark_dbm_set():
+    """Ddm test"""
+
+    start = time.time()
+    [orig.set_bulk_kv(req, db=0) for req in requests]
+    print(
+        'orig set_bulk qps:',
+        int(NUM_REQUESTS * NUM_BULK / (time.time() - start))
+    )
+
+
+def benchmark_dbm_get():
+    """Dbm test"""
+
+    for req in requests:
+        for k, v in req.items():
+            dbm_file[k] = v
+
+    start = time.time()
+    for req in requests:
+        for k, v in req.items():
+            req[k] = dbm_file[k]
+    dbm_file.sync()
+    print(
+        'dmb get qps:',
+        int(NUM_REQUESTS * NUM_BULK / (time.time() - start))
+    )
+
+
 def benchmark_orig_set_bulk():
     """Original bulk test"""
 
@@ -158,11 +192,13 @@ def benchmark_orig_set_bulk():
         int(NUM_REQUESTS * NUM_BULK / (time.time() - start))
     )
 
-
+print("Starting")
 benchmark_get_bulk()
 benchmark_set_bulk()
 benchmark_batch_get_bulk()
 benchmark_orig_get_bulk()
 benchmark_orig_set_bulk()
-# os.unlink("test.kch")
+benchmark_dbm_get()
+os.unlink("test.kch")
+os.unlink("test.dbm")
 # pylama:ignore=W0106
